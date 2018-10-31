ce1c4716063c7388b8a84bef9a79b97d6ce042b1
BrianARuff <thelonewolf198927@gmail.com>
2018-10-31 12:48:25 -0400

    changed log msg from ser to API

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 8000;
 const instance = server.listen(port, () =>
-	console.log(`\n=== Server running on port: ${instance.address().port} ===\n`)
+	console.log(`\n=== API running on port: ${instance.address().port} ===\n`)
 );
