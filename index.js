4d15c4fdb9c63a1f2ed11f46dc311feb85c25001
BrianARuff <thelonewolf198927@gmail.com>
2018-10-31 13:07:54 -0400

    fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = 8000;
+const port = process.env.PORT;
 const instance = server.listen(port, () =>
 	console.log(`\n=== API running on port: ${instance.address().port} ===\n`)
 );
